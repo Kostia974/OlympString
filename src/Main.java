eefe2d375bb618f323861ed1b976eb173f4f0554
kostia <you@example.com>
2022-01-14 18:45:09 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    private static Object n;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+
+        System.out.println(s.substring(4, 10));
+        System.out.println(1,2, s-1);
+        System.out.println(s-5);
+        System.out.println(n-4);
+        for (int i = 0; i < 2; i++) {
+            if (i % 2) ;
+            System.out.print(i);
+            System.out.println(s);
+        }
+    }
+}
